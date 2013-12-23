22a3ce864d1d600059b57580264f69065eba817c
Ken Ko <git@yaksok.net>
2013-12-23 06:57:36 -0800

    print csv to file

--- build-csum-table.py
+++ build-csum-table.py
@@ -13,4 +13,17 @@ flist = ['test/2011-01-01/a.JPG']
 
 clist = [(fname, "".join('%02x' % ord(c) for c in hashfile(open(fname, 'rb'), hashlib.sha256()))) for fname in flist]
 
-print clist
+def hashdir():
+    f = open('checksums', 'w')
+
+    for pairs in clist:
+        s = "%(name)s,%(csum)s" % {'name':pairs[0], 'csum':pairs[1]}
+        print s
+        # does (file,csum) already exist in file?
+        # do we care?
+        f.write(s)
+
+    f.close()
+
+
+hashdir()
