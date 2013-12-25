674653ea711ef09b9f5591c5104781a567ff5795
Ken Ko <git@yaksok.net>
2013-12-25 13:46:36 -0700

    basically works. naively

--- build-csum-table.py
+++ build-csum-table.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import hashlib
+import os
+import argparse
 
 def hashfile(file, hasher, blksz=65536):
     buf = file.read(blksz)
@@ -9,21 +11,37 @@ def hashfile(file, hasher, blksz=65536):
         buf = file.read(blksz)
     return hasher.digest()
 
-flist = ['test/2011-01-01/a.JPG']
+def populate_flist():
+    flist = []
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith((extension)):
+                path = os.path.join(root,file)
+                flist.append(path)
+    return flist
 
-clist = [(fname, "".join('%02x' % ord(c) for c in hashfile(open(fname, 'rb'), hashlib.sha256()))) for fname in flist]
+def flist_to_clist(flist=[]):
+    return [(fname, "".join('%02x' % ord(c) for c in hashfile(open(fname, 'rb'), hashlib.sha256()))) for fname in flist]
 
-def hashdir():
-    f = open('checksums', 'w')
 
-    for pairs in clist:
-        s = "%(name)s,%(csum)s" % {'name':pairs[0], 'csum':pairs[1]}
-        print s
-        # does (file,csum) already exist in file?
-        # do we care?
+def handle_clist(clist=[]):
+    for pair in clist:
+        dirpath = os.path.dirname(pair[0])
+        # TODO cksumfile per file
+        cksum_file = os.path.join(dirpath,cksumfile)
+        if (os.path.exists(cksum_file) == False):
+            f = open(cksum_file, 'w')
+        else:
+            f = open(cksum_file, 'a')
+        s = "%(name)s,%(csum)s\n" % {'name':pair[0], 'csum':pair[1]}
         f.write(s)
+        f.close()
 
-    f.close()
+
+cksumfile='.cksum'
+extension='.JPG'
+flist = populate_flist()
+clist = flist_to_clist(flist)
+handle_clist(clist)
 
 
-hashdir()
