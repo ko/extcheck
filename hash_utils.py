97bcd980f43986942dfc609af43fc4127b3842f9
Ken Ko <git@yaksok.net>
2015-02-24 21:49:57 -0800

    replace it all

--- hash_utils.py
+++
@@ -1,8 +0,0 @@
-def hashfile(file, hasher, blksz=65536):
-    buf = file.read(blksz)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = file.read(blksz)
-    return hasher.digest()
-
-
